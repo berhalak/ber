33f34810b5e243be336db92dc1c9a874adfb2d3f
berhalak <jaroslaw.sadzinski@gmail.com>
2019-09-28 00:18:42 +0200

    Fixing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { readdirSync, statSync, readFileSync, writeFileSync, renameSync } = require('fs')
+const { readdirSync, statSync, readFileSync, writeFileSync, renameSync, existsSync, mkdirSync } = require('fs')
 const { join, resolve, relative, basename } = require('path')
 
 const { exec, execSync } = require('child_process');
@@ -21,39 +21,43 @@ class Context {
         exec(`npm pack ${abs}`);
         let name = info.name;
         name = name.replace("/", "_").replace("@", "");
-        name = `${name}_${info.version}.tgs`;
+        name = `${name}-${info.version}.tgz`;
 
         let oldPath = name;
         let newPath = join("local_modules", name);
+
+        if (!existsSync("local_modules")) {
+            mkdirSync("local_modules");
+        }
 
         renameSync(oldPath, newPath);
 
         return newPath;
     }
 
-    readPackageName(/** @type { string} */ folder) {
+    readPackageInfo(/** @type { string} */ folder) {
         let fileName = join(folder, "package.json");
-        let content = readFileSync(fileName).toString();
+        let content = JSON.parse(readFileSync(fileName).toString());
         return content;
     }
 
     addPackageAsBerToLocalPJ(/** @type { string} */ folder, /** @type { string} */ packageName) {
         let local_package_json = join(this.current, "package.json");
-        let content = readFileSync(local_package_json).toString();
+        let content = JSON.parse(readFileSync(local_package_json).toString());
         if (!content.ber) {
             content.ber = {};
         }
+        folder = relative(this.current, folder);
         content.ber[packageName] = folder;
         writeFileSync(local_package_json, JSON.stringify(content, null, 2));
     }
 
-    installFromFolder(folder) {
+    installFromFolder(/** @type { string} */ folder) {
+
         let info = this.readPackageInfo(folder);
         this.addPackageAsBerToLocalPJ(folder, info.name);
         let tarFile = this.packPackage(folder, info);
-        this.moveFile(tarFile, "./local_modules");
-        let name = this.getFileName(tarFile);
-        this.installModule('./local_modules/' + name);
+        this.installModule(tarFile);
     }
 }
 
@@ -63,6 +67,9 @@ exports.main = function (/** @type { Array} */ args) {
     // install local folder to package json
     if (args.length == 2 && args[0] == "i") {
         let context = new Context(process.cwd());
+        console.log("Instaling package... " + args[1])
         context.installFromFolder(args[1]);
+    } else {
+        console.log("usage: ber i <path_to_package>");
     }
 }

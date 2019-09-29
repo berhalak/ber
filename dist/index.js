9aa9b2c267edab475a43cf2f1874c4e55694f05c
berhalak <jaroslaw.sadzinski@gmail.com>
2019-09-29 02:02:37 +0200

    Moving to typescript

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,45 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_extra_1 = __importDefault(require("fs-extra"));
 const path_1 = __importDefault(require("path"));
 const child_process_1 = require("child_process");
-function run(command) {
+const rimraf_1 = __importDefault(require("rimraf"));
+async function readJson(file) {
+    let content = JSON.parse((await fs_extra_1.default.readFile(file)).toString());
+    return content;
+}
+async function writeJson(file, data) {
+    await fs_extra_1.default.writeFile(file, JSON.stringify(data, null, 2));
+}
+async function readConfig(path) {
+    let file = path_1.default.resolve(path_1.default.join(path, "package.json"));
+    return await readJson(file);
+}
+async function writeConfig(path, data) {
+    let file = path_1.default.resolve(path_1.default.join(path, "package.json"));
+    await writeJson(file, data);
+}
+class Directory {
+    static remove(path) {
+        return new Promise((resolve, reject) => {
+            rimraf_1.default(path, err => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
+function run(command, cwd) {
+    if (cwd) {
+        console.log(`[${cwd}] ${command}`);
+    }
+    else {
+        console.log(command);
+    }
     return new Promise(function (resolve, reject) {
-        child_process_1.exec(command, (error, stdout, stderr) => {
+        child_process_1.exec(command, { cwd }, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
@@ -114,22 +150,158 @@ class Context {
         }
     }
 }
+class Npm {
+    static async install(file, cwd) {
+        file = path_1.default.relative(cwd, file);
+        await run(`npm install ${file}`, cwd);
+    }
+}
+class Tgz {
+    constructor(name, version, folder) {
+        this.name = name;
+        this.version = version;
+        this.folder = folder;
+        this.fileName = name.replace("@", "").replace("/", "-") + "-" + version + ".tgz";
+    }
+    path() {
+        return path_1.default.resolve(this.folder, this.fileName);
+    }
+    async copyTo(folder) {
+        let newPath = path_1.default.resolve(path_1.default.join(folder, this.fileName));
+        console.log(`copy ${this.path()} -> ${newPath}`);
+        await fs_extra_1.default.copyFile(this.path(), newPath);
+        return new Tgz(this.name, this.version, folder);
+    }
+    async exists() {
+        return await fs_extra_1.default.pathExists(this.path());
+    }
+}
+const local_modules = ".local_modules";
+const dotOutput = ".out";
+class Package {
+    constructor(path) {
+        this.info = null;
+        this.path = path_1.default.resolve(path);
+    }
+    async install(pkg) {
+        if (pkg) {
+            await pkg.clean();
+            await this.addToBer(pkg);
+            await this.installPackage(pkg);
+        }
+        else {
+            let references = await this.references();
+            for (let ref of references) {
+                await ref.clean();
+            }
+            for (let ref of references) {
+                await this.installPackage(ref);
+            }
+        }
+    }
+    async references() {
+        let myConfig = await readConfig(this.path);
+        let result = [];
+        for (let name in myConfig.ber) {
+            let path = myConfig.ber[name];
+            path = path_1.default.resolve(path_1.default.join(this.path, path));
+            result.push(new Package(path));
+        }
+        return result;
+    }
+    async installPackage(pkg) {
+        for (let ref of await pkg.references()) {
+            await this.installPackage(ref);
+        }
+        let pkgOutput = await pkg.output();
+        let myBinary = await pkgOutput.copyTo(await this.localRepo());
+        await Npm.install(myBinary.path(), this.path);
+    }
+    async localRepo() {
+        let path = path_1.default.resolve(path_1.default.join(this.path, local_modules));
+        if (!await fs_extra_1.default.pathExists(path)) {
+            await fs_extra_1.default.mkdir(path);
+        }
+        return path;
+    }
+    async output() {
+        let name = await this.name();
+        let version = await this.version();
+        let outPath = path_1.default.resolve(path_1.default.join(this.path, dotOutput));
+        let tgz = new Tgz(name, version, outPath);
+        if (await tgz.exists()) {
+            return tgz;
+        }
+        await this.build();
+        return tgz;
+    }
+    async build() {
+        console.log("Building package " + await this.name());
+        let references = await this.references();
+        for (let ref of references) {
+            let pkgOutput = await ref.output();
+            let myBinary = await pkgOutput.copyTo(await this.localRepo());
+            await Npm.install(myBinary.path(), this.path);
+        }
+        if (!this.info) {
+            await this.readInfo();
+        }
+        if (this.info.scripts['build']) {
+            await run('npm run build', this.path);
+        }
+        let outputPath = path_1.default.resolve(this.path, dotOutput);
+        if (!await fs_extra_1.default.pathExists(outputPath)) {
+            await fs_extra_1.default.mkdir(outputPath);
+        }
+        await run("npm pack ../", outputPath);
+        console.log("Build finish " + await this.name());
+    }
+    async clean() {
+        let outDir = path_1.default.resolve(path_1.default.join(this.path, dotOutput));
+        await Directory.remove(outDir);
+        let references = await this.references();
+        for (let ref of references) {
+            await ref.clean();
+        }
+    }
+    async addToBer(pkg) {
+        let config = await readConfig(this.path);
+        let packagePath = pkg.path;
+        let packageName = await pkg.name();
+        config.ber = config.ber || {};
+        config.ber[packageName] = path_1.default.relative(this.path, packagePath);
+        await writeConfig(this.path, config);
+    }
+    async name() {
+        if (!this.info) {
+            await this.readInfo();
+        }
+        return this.info.name;
+    }
+    async version() {
+        if (!this.info) {
+            await this.readInfo();
+        }
+        return this.info.version;
+    }
+    async readInfo() {
+        if (!this.info) {
+            this.info = await readConfig(this.path);
+        }
+    }
+}
 exports.main = async function (/** @type { Array} */ args) {
     // install local folder to package json
     if (args.length == 2 && args[0] == "i") {
-        let context = new Context(process.cwd());
+        let self = new Package(process.cwd());
+        let ref = args[1];
         console.log("Instaling package... " + args[1]);
-        await context.installFromFolder(args[1]);
+        await self.install(new Package(ref));
     }
     else if (args.length == 1 && args[0] == "i") {
-        let context = new Context(process.cwd());
+        let self = new Package(process.cwd());
         console.log("Restoring packages... ");
-        await context.update();
-    }
-    else if (args.length == 1 && args[0] == "b") {
-        let context = new Context(process.cwd());
-        console.log("Building local packages... ");
-        await context.buildDeps();
+        await self.install();
     }
     else {
         console.log("usage: ber i [<path_to_package>]");
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,51 @@
-
 import File from "fs-extra"
 import Path from "path"
 import { exec, execSync, spawnSync } from 'child_process';
+import rimraf from "rimraf";
 
-function run(command: string) {
+async function readJson(file: string): Promise<any> {
+    let content = JSON.parse((await File.readFile(file)).toString());
+    return content;
+}
+
+async function writeJson(file: string, data: any): Promise<void> {
+    await File.writeFile(file, JSON.stringify(data, null, 2));
+}
+
+async function readConfig(path: string): Promise<PackageJson> {
+    let file = Path.resolve(Path.join(path, "package.json"));
+    return await readJson(file);
+}
+
+async function writeConfig(path: string, data: PackageJson): Promise<void> {
+    let file = Path.resolve(Path.join(path, "package.json"));
+    await writeJson(file, data);
+}
+
+
+
+class Directory {
+    public static remove(path: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            rimraf(path, err => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            })
+        })
+    }
+}
+
+function run(command: string, cwd?: string) {
+    if (cwd) {
+        console.log(`[${cwd}] ${command}`);
+    } else {
+        console.log(command);
+    }
     return new Promise(function (resolve, reject) {
-        exec(command, (error, stdout, stderr) => {
+        exec(command, { cwd }, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
@@ -134,22 +174,187 @@ class Context {
     }
 }
 
+class Npm {
+    static async install(file: any, cwd: string) {
+        file = Path.relative(cwd, file);
+        await run(`npm install ${file}`, cwd);
+    }
+
+}
+
+class Tgz {
+    fileName: string;
+    constructor(public name: string, public version: string, public folder: string) {
+        this.fileName = name.replace("@", "").replace("/", "-") + "-" + version + ".tgz";
+    }
+    path(): any {
+        return Path.resolve(this.folder, this.fileName);
+    }
+
+    async copyTo(folder: string): Promise<Tgz> {
+        let newPath = Path.resolve(Path.join(folder, this.fileName));
+        console.log(`copy ${this.path()} -> ${newPath}`);
+        await File.copyFile(this.path(), newPath);
+        return new Tgz(this.name, this.version, folder);
+
+    }
+
+    async exists() {
+        return await File.pathExists(this.path());
+    }
+}
+
+const local_modules = ".local_modules";
+const dotOutput = ".out";
+
+class Package {
+    public path: string;
+    constructor(path: string) {
+        this.path = Path.resolve(path);
+    }
+
+    public async install(): Promise<void>;
+    public async install(pkg: Package): Promise<void>
+    public async install(pkg?: Package) {
+        if (pkg) {
+            await pkg.clean();
+            await this.addToBer(pkg);
+            await this.installPackage(pkg);
+        } else {
+            let references: Package[] = await this.references();
+            for (let ref of references) {
+                await ref.clean();
+            }
+            for (let ref of references) {
+                await this.installPackage(ref);
+            }
+        }
+    }
+
+    private async references(): Promise<Package[]> {
+        let myConfig = await readConfig(this.path);
+        let result: Package[] = [];
+        for (let name in myConfig.ber) {
+            let path = myConfig.ber[name];
+            path = Path.resolve(Path.join(this.path, path));
+            result.push(new Package(path));
+        }
+        return result;
+    }
+
+
+    private async installPackage(pkg: Package) {
+        for (let ref of await pkg.references()) {
+            await this.installPackage(ref);
+        }
+        let pkgOutput = await pkg.output();
+        let myBinary = await pkgOutput.copyTo(await this.localRepo());
+        await Npm.install(myBinary.path(), this.path);
+    }
+
+    private async localRepo() {
+        let path = Path.resolve(Path.join(this.path, local_modules));
+        if (!await File.pathExists(path)) {
+            await File.mkdir(path);
+        }
+        return path;
+    }
+
+    private async output(): Promise<Tgz> {
+        let name = await this.name();
+        let version = await this.version();
+        let outPath = Path.resolve(Path.join(this.path, dotOutput));
+        let tgz = new Tgz(name, version, outPath);
+
+        if (await tgz.exists()) {
+            return tgz;
+        }
+        await this.build();
+        return tgz;
+    }
+
+    private async build() {
+        console.log("Building package " + await this.name());
+        let references: Package[] = await this.references();
+
+        for (let ref of references) {
+            let pkgOutput = await ref.output();
+            let myBinary = await pkgOutput.copyTo(await this.localRepo());
+            await Npm.install(myBinary.path(), this.path);
+        }
+
+        if (!this.info) {
+            await this.readInfo();
+        }
+
+        if (this.info.scripts['build']) {
+            await run('npm run build', this.path);
+        }
+
+        let outputPath = Path.resolve(this.path, dotOutput);
+        if (!await File.pathExists(outputPath)) {
+            await File.mkdir(outputPath);
+        }
+
+        await run("npm pack ../", outputPath);
+        console.log("Build finish " + await this.name());
+    }
+
+    private async clean(): Promise<void> {
+        let outDir = Path.resolve(Path.join(this.path, dotOutput));
+        await Directory.remove(outDir);
+        let references: Package[] = await this.references();
+        for (let ref of references) {
+            await ref.clean();
+        }
+    }
+
+    private async addToBer(pkg: Package) {
+        let config = await readConfig(this.path);
+
+        let packagePath = pkg.path;
+        let packageName = await pkg.name();
+        config.ber = config.ber || {};
+        config.ber[packageName] = Path.relative(this.path, packagePath);
+        await writeConfig(this.path, config);
+    }
+
+    private info: PackageJson = null;
+
+    public async name() {
+        if (!this.info) {
+            await this.readInfo();
+        }
+        return this.info.name;
+    }
+
+    public async version() {
+        if (!this.info) {
+            await this.readInfo();
+        }
+        return this.info.version;
+    }
+
+    private async readInfo() {
+        if (!this.info) {
+            this.info = await readConfig(this.path);
+        }
+    }
+}
+
 
 
 exports.main = async function (/** @type { Array} */ args: any[]) {
     // install local folder to package json
     if (args.length == 2 && args[0] == "i") {
-        let context = new Context(process.cwd());
+        let self = new Package(process.cwd());
+        let ref = args[1] as string;
         console.log("Instaling package... " + args[1])
-        await context.installFromFolder(args[1]);
+        await self.install(new Package(ref));
     } else if (args.length == 1 && args[0] == "i") {
-        let context = new Context(process.cwd());
+        let self = new Package(process.cwd());
         console.log("Restoring packages... ");
-        await context.update();
-    } else if (args.length == 1 && args[0] == "b") {
-        let context = new Context(process.cwd());
-        console.log("Building local packages... ");
-        await context.buildDeps();
+        await self.install();
     } else {
         console.log("usage: ber i [<path_to_package>]");
     }
